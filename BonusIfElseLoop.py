0a54037d593bcd252ed9db69d68e46df41534eb4
dolivo1980 <dolivo1980@gmail.com>
2017-07-25 19:44:20 -0400

    Add files via upload

---
+++ BonusIfElseLoop.py
@@ -0,0 +1,39 @@
+#Version 1: with if else
+def greeting():
+    name = input("Please enter you name: ")
+    print("Nice to meet you ", name)
+    print(name, end="")
+    color = input(", what is your favorite color: ")
+    print(color, end="")
+    print("? That's a nice color.")
+    print(name, end="")
+    food = input(", what is your favorite food: ")
+    print(food, end="")
+    print(" sounds yummy.")
+    print(name, end="")
+    goToGo = input(", do you have to go? (yes/no) ")
+    if(goToGo == "yes"):
+        print("Goodbye ", name)
+    else:
+        greeting()
+
+greeting()
+
+#Version 2: with if else
+print("This is where version 2 starts")
+goToStart = "no"
+while goToStart == "no":
+    name = input("Please enter you name: ")
+    print("Nice to meet you ", name)
+    print(name, end="")
+    color = input(", what is your favorite color: ")
+    print(color, end="")
+    print("? That's a nice color.")
+    print(name, end="")
+    food = input(", what is your favorite food: ")
+    print(food, end="")
+    print(" sounds yummy.")
+    print(name, end="")
+    goToStart = input(", do you have to go? (yes/no) ")
+
+print("Goodbye ")
---
+++ FarOrCelRange.py
@@ -0,0 +1,7 @@
+a=0
+while(a <= 100):
+    CelTemp = a
+    FahTemp = (CelTemp * 1.8) + 32
+    print(" Celsius: ", CelTemp, " Fahrenheit: ", FahTemp)
+    a+=5
+    
