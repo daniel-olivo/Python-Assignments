a522d896b2d285f9dbc05833e1445b9847d13a8c
dolivo1980 <dolivo1980@gmail.com>
2017-07-27 20:03:42 -0400

    Add files via upload

--- Assignments/BonusIfElseLoop.py
+++ Assignments/BonusIfElseLoop.py
@@ -1,34 +1,35 @@
 #Version 1: with if else
+'''
 def greeting():
     name = input("Please enter you name: ")
     print("Nice to meet you ", name)
-    print(name, end="")
-    color = input(", what is your favorite color: ")
-    print(color, end="")
-    print("? That's a nice color.")
-    print(name, end="")
-    food = input(", what is your favorite food: ")
-    print(food, end="")
-    print(" sounds yummy.")
-    print(name, end="")
-    goToGo = input(", do you have to go? (yes/no) ")
+    #print(name, end="")
+    color = input(name, ", what is your favorite color: ")
+    #print(color, end="")
+    print(color, "? That's a nice color.")
+    #print(name, end="")
+    food = input(name, ", what is your favorite food: ")
+    #print(food, end="")
+    print(name, " sounds yummy.")
+    #print(name, end="")
+    goToGo = input(name, ", do you have to go? (yes/no) ")
     if(goToGo == "yes"):
         print("Goodbye ", name)
     else:
         greeting()
 
 greeting()
-
+'''
 #Version 2: with if else
 print("This is where version 2 starts")
 goToStart = "no"
 while goToStart == "no":
     name = input("Please enter you name: ")
     print("Nice to meet you ", name)
-    color = input(name, ", what is your favorite color: ")
-    print(color, "? That's a nice color.")
-    food = input(name, ", what is your favorite food: ")
-    print(food," sounds yummy.")
-    goToStart = input(name,", do you have to go? (yes/no) ")
+    color = input("{},what is your favorite color: ".format(name))
+    print("{}?, That's a nice color.".format(color))
+    food = input("{}, what is your favorite food: ".format(name))
+    print("{} sounds yummy.".format(food))
+    goToStart = input("{}, do you have to go? (yes/no) ".format(name))
 
 print("Goodbye ")
